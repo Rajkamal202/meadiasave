ac23603dd8be007e582c9e2993e8ab437ed3b480
Rajkamal Rao <122449211+Rajkamal202@users.noreply.github.com>
2025-06-29 15:05:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
 
 const app = express();
 app.use(cors());
